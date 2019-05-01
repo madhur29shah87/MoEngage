2e5f8a7ef307599f375b5849885b272032c059e0
madhur29shah87 <madhur29.shah@gmail.com>
2019-05-01 15:27:39 +0530

    update dialog to display content according to his choise of edit/delete. code cleanup

--- moengage.js
+++ moengage.js
@@ -1,47 +1,83 @@
 var mainDiv;
 var campaignListData;
 var recordsLength = 0;
+var modal;
+var currentElement;
+var currentCampaignId;
 
-function loadCanpaignList() {
+
+function pageLoadInit() {
     var ul = document.getElementById('ulPage');
+    // Adding listner on Pagination click to load data accordingly
     ul.addEventListener('click', function navigate(event) {
-        populateTemplate(parseInt(event.target.innerText)-1);
+        populateCampignTable(parseInt(event.target.innerText) - 1);
     });
-    var modal = document.getElementById('myModal');
+
+    // Modal dom for edit and delete confirmaton 
+    modal = document.getElementById('myModal');
+
     var xhttp = new XMLHttpRequest();
     var pageNumber = 0;
-
-    var paginationList = document.getElementById('ulPage');
-
+    // pageination container
+    var paginationContainer = document.getElementById('ulPage');
+    // Ajax listner when response is received from server
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            mainDiv = document.getElementById("tBody");
+            
+            // Get list of campaign from response
             campaignListData = JSON.parse(this.responseText);
             recordsLength = campaignListData.length;
-            var pagesCount = (recordsLength%10 == 0) ? recordsLength/10 : parseInt(recordsLength/10)+1; 
-            for (var i = 0; i < pagesCount ; i++) {
+            // to decide number of pages .. below logic take care of last page if items less than 10
+            var pagesCount = (recordsLength % 10 == 0) ? parseInt(recordsLength / 10) : parseInt(recordsLength / 10) + 1;
+            for (var i = 0; i < pagesCount; i++) {
+
+                // Populate pagination DOM
                 var page = document.createElement('li');
                 page.classList.add('page-item');
                 var pageLink = document.createElement('a');
                 pageLink.classList.add('page-link');
-                pageLink.innerHTML = i+1;
+                pageLink.innerHTML = i + 1;
                 pageLink.setAttribute('href', '#');
                 page.appendChild(pageLink);
-                paginationList.appendChild(page);
+                paginationContainer.appendChild(page);
             }
-            populateTemplate(pageNumber);
+            // Defaulting to page 0 i.e 1st page
+            populateCampignTable(pageNumber);
         }
     };
+    // defining Ajax request
     xhttp.open("GET", "data.json", true);
+    //  trigger Ajax request to server in this case locale data.json
     xhttp.send();
-    var span = document.getElementsByClassName("close")[0];
 
+    // Added listner for modal close button on top right corner
+    var span = document.getElementsByClassName("close")[0];
     span.onclick = function () {
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
-    var modal = document.getElementById('myModal');
+    // Added listner to get current element which user interacted for either edit or delete
+    document.addEventListener('current-element', function (event) {
+        currentElement = event.detail.element;
+        currentCampaignId = event.detail.campaignId;
+    });
+
+    var modalButton = document.getElementById("modalButton");
+
+    // Edit modal dialog , same dialog is used for delete confirmation conditionally 
+    modalButton.onclick = function (event) {
+        if (event.currentTarget.innerHTML == 'Delete') {
+            currentElement.parentElement.parentElement.remove();
+        } else if (event.currentTarget.innerHTML == 'Change') {
+            currentElement.parentElement.parentElement.querySelector('#'+currentCampaignId).innerHTML = 
+            event.currentTarget.parentElement.querySelector('#editCampaign').value;
+            currentElement.parentElement.parentElement.querySelector('#date').innerHTML = new Date();
+        }
+        modal.style.display = "none";
+    }
+
+    
+    // When the user clicks anywhere outside of the modal, close modal
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
@@ -49,21 +85,23 @@ function loadCanpaignList() {
     }
 }
 
-function populateTemplate(pageNumber) {
-    // Pagination logic
+function populateCampignTable(pageNumber) {
+    // Clears dom according to pagination.. no data is appended to older DOM 
+    // same DOM is reused
+    mainDiv = document.getElementById("tBody");
     while (mainDiv.hasChildNodes()) {
         mainDiv.removeChild(mainDiv.firstChild);
-     }
-    var x = (pageNumber == 0) ? 0 : (pageNumber * 10 - 1);
+    }
+    var x = (pageNumber == 0) ? 0 : (pageNumber * 10);
     var upperLimit = (x + 10 <= recordsLength) ? x + 10 : recordsLength;
     for (x; x < upperLimit; x++) {
         var checkBox = document.createElement("INPUT");
         checkBox.setAttribute("type", "checkbox");
         var editIcon = document.createElement("i");
-        // editIcon.setAttribute('class', 'fa fa-pencil-square-o');
         editIcon.classList.add("icon");
         editIcon.classList.add("fa");
         editIcon.classList.add("fa-pencil-square-o");
+        editIcon.setAttribute("id", campaignListData[x].company);
         checkBox.setAttribute("type", "checkbox");
         var deleteIcon = document.createElement("i");
         // editIcon.setAttribute('class', 'fa fa-pencil-square-o');
@@ -78,18 +116,45 @@ function populateTemplate(pageNumber) {
         var campaignTd3 = document.createElement('td');
         var campaignTd4 = document.createElement('td');
 
-        campaignTh.setAttribute('scope', 'row');
-        campaignTh.appendChild(checkBox);
+        // campaignTr.setAttribute('id', campaignListData[x].company);
+        // campaignTh.setAttribute('scope', 'row');
+        // campaignTh.appendChild(checkBox);
         campaignTd1.innerHTML = campaignListData[x].company;
+        campaignTd1.setAttribute('id', campaignListData[x].company);
         campaignTd2.innerHTML = campaignListData[x].type;
-        campaignTd3.innerHTML = "v";
+        campaignTd3.innerHTML = "-";
+        campaignTd3.setAttribute('id','date');
         campaignTd4.classList.add('align-content-center');
         campaignTd4.appendChild(editIcon);
-        editIcon.onclick = function () {
+        editIcon.onclick = function (event) {
             modal.style.display = "block";
+            modal.querySelector('#modalButton').innerHTML = "Change";
+            modal.querySelector('#editCampaign').style.display = "block";
+            modal.querySelector('#editCampaign').focus();
+            modal.querySelector('.modal-header h2').innerHTML = "Edit Campaign Name";
+            this.dispatchEvent(new CustomEvent('current-element', {
+                bubbles: true,
+                detail: {
+                    element: event.currentTarget,
+                    campaignId: event.target.id
+                }
+            }));
         }
         campaignTd4.appendChild(deleteIcon);
-        campaignTr.appendChild(campaignTh);
+        deleteIcon.onclick = function (event) {
+            modal.style.display = "block";
+            modal.querySelector('#modalButton').innerHTML = "Delete";
+            modal.querySelector('#modalButton').focus();
+            modal.querySelector('#editCampaign').style.display = "none";
+            modal.querySelector('.modal-header h2').innerHTML = "Delete Campaign Name";
+            this.dispatchEvent(new CustomEvent('current-element', {
+                bubbles: true,
+                detail: {
+                    element: event.currentTarget
+                }
+            }));
+        }
+        // campaignTr.appendChild(campaignTh);
         campaignTr.appendChild(campaignTd1);
         campaignTr.appendChild(campaignTd2);
         campaignTr.appendChild(campaignTd3);
@@ -97,3 +162,21 @@ function populateTemplate(pageNumber) {
         mainDiv.appendChild(campaignTr);
     }
 }
+
+var timer = 0;
+// delay the search for 400ms while user is typing, increase it to 1000 if our user is immense slow
+function delaySearch(event) {
+      clearTimeout(timer);
+      var searchResult = document.getElementById('searchResult');
+      timer = setTimeout(function () {
+        var searchText = event.value;
+        for(var i = 0; i<recordsLength;i++){
+            if(campaignListData[i].company.toLowerCase() == searchText.toLowerCase()){
+                searchResult.innerHTML = "Company Found - <i>" + campaignListData[i].company + "</i> Type - <i> " + campaignListData[i].type +"</i>";
+                break;
+            } else {
+                searchResult.innerHTML = 'No search result';
+            }
+        }
+      }, 400);
+  }
